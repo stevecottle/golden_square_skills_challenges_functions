593c4256d127d5de9a29478909434f51a82b8758
Steve Cottle <stevocottle@gmail.com>
2025-04-10 14:41:02 +0100

    count_words challenge WIP

---
+++ lib/count_words.py
@@ -0,0 +1,14 @@
+"""
+A function called count_words that takes a 
+string as an argument and returns the number 
+of words in that string.
+"""
+
+# For test 1-2
+# def count_words(string):
+#     return string
+
+# For tests 3-??
+def count_words(string):
+    wordcount = len(string.split())
+    return wordcount
---
+++ tests/test_count_words.py
@@ -0,0 +1,32 @@
+from lib.count_words import *
+
+"""
+count_words returns an input of an empty string
+"""
+
+# def test_function_returns_empty_string():
+#     result = count_words("")
+#     assert result == ""
+
+
+# """
+# count_words returns an input of a string of words
+# """
+
+# def test_function_returns_string_of_words():
+#     result = count_words("first morning")
+#     assert result == "first morning"
+
+
+"""
+count_words can return the count of a one word string
+"""
+
+def test_function_returns_count_of_one_word_string():
+
+
+
+"""
+count_words return the count a 2 word string
+"""
+
